6dc15e10fca95b86758484133bd532a173767fac
Charan <icharan720@gmail.com>
2025-10-30 08:52:52 +0530

    Fix Bastion.tf

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "bastion_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = local.my_public_ip
+    cidr_blocks = [local.my_public_ip]
   }
 
   # Allow all outbound
